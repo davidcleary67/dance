684e365f9b49b9200d9ce521fed07f0433ee18e2
davidcleary67 <87801023+davidcleary67@users.noreply.github.com>
2022-11-07 13:32:13 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,156 +1,126 @@
 let moves: Image[] = []
 let dance: number[] = []
+let speed = 0
 let move = 0
 function createMoves () {
     moves = [
     images.createImage(`
-        . # . # .
-        . # . # .
+        . . # . .
         . # # # .
-        . # . # .
+        # . # . #
+        . . # . .
         . # . # .
         `),
     images.createImage(`
         # . # . .
+        . # # # .
+        . . # . #
+        . . # . .
+        . # . # .
+        `),
+    images.createImage(`
+        . . # . #
+        . # # # .
         # . # . .
+        . . # . .
+        . # . # .
+        `),
+    images.createImage(`
+        # . # . #
+        . # # # .
+        . . # . .
+        . . # . .
+        . # . # .
+        `),
+    images.createImage(`
+        . # . . .
         # # # . .
-        # . # . .
+        . # . # .
+        . # . . .
         # . # . .
         `),
     images.createImage(`
-        . . # . #
-        . . # . #
+        . . . # .
         . . # # #
+        . # . # .
+        . . . # .
         . . # . #
-        . . # . #
         `),
     images.createImage(`
-        . # . # .
         . # # # .
-        . # . # .
+        # . # . #
+        . . # . .
         . # . # .
         . . . . .
         `),
     images.createImage(`
         . . . . .
-        . # . # .
-        . # . # .
-        . # # # .
-        . # . # .
-        `),
-    images.createImage(`
-        . # # # .
-        . # . . .
-        . # # . .
-        . # . . .
-        . # . . .
-        `),
-    images.createImage(`
-        # # # . .
-        # . . . .
-        # # . . .
-        # . . . .
-        # . . . .
-        `),
-    images.createImage(`
-        . . # # #
         . . # . .
-        . . # # .
-        . . # . .
+        . # # # .
+        # . # . #
         . . # . .
         `),
     images.createImage(`
+        # . # # .
         . # . . .
-        . # # . .
-        . # . . .
-        . # . . .
-        . . . . .
-        `),
-    images.createImage(`
-        . . . . .
-        . # # # .
-        . # . . .
-        . # # . .
-        . # . . .
-        `),
-    images.createImage(`
-        . # # . .
-        . # . # .
-        . # # . .
-        . # . # .
-        . # # . .
-        `),
-    images.createImage(`
-        # # . . .
         # . # . .
-        # # . . .
-        # . # . .
-        # # . . .
+        # . . # #
+        . . . # .
         `),
     images.createImage(`
-        . . # # .
+        . # # . #
+        . . . # .
         . . # . #
-        . . # # .
-        . . # . #
-        . . # # .
-        `),
-    images.createImage(`
-        . # . # .
-        . # # . .
-        . # . # .
-        . # # . .
-        . . . . .
-        `),
-    images.createImage(`
-        . . . . .
-        . # # . .
-        . # . # .
-        . # # . .
-        . # . # .
+        # # . . #
+        . # . . .
         `)
     ]
 }
-input.onButtonPressed(Button.A, function () {
+function doStartup () {
     basic.showLeds(`
-        # . # . .
+        . . # . .
         . # # # .
-        . . # . #
+        # . # . #
         . . # . .
         . # . # .
         `)
-    basic.pause(3000)
+    basic.pause(2000)
+    basic.clearScreen()
+    basic.pause(1000)
+}
+input.onButtonPressed(Button.A, function () {
+    doStartup()
     dance = [
+    200,
     1,
     2,
     3,
     4,
+    5,
     6,
     7,
     8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14
+    9
     ]
     doDance()
 })
 function doDance () {
     createMoves()
+    speed = 1000
     for (let index = 0; index <= dance.length; index++) {
         if (index < dance.length) {
             move = dance[index]
-            if (move < 5) {
+            if (move < 10) {
                 moves[0].showImage(0)
-            } else if (move < 10) {
-                moves[5].showImage(0)
+                basic.pause(speed)
+                moves[move].showImage(0)
+                basic.pause(speed)
             } else {
-                moves[10].showImage(0)
+                speed = move
             }
         }
-        basic.pause(1000)
-        moves[move].showImage(0)
-        basic.pause(1000)
     }
+    moves[0].showImage(0)
+    basic.pause(1000)
+    basic.clearScreen()
 }
